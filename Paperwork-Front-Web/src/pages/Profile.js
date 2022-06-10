a6a211e0c72a8ff88640e45e8fc48ea21a86d2a2
Dylan Faure <dylan.faure@pm-sa.fr>
2022-06-10 18:42:11 +0200

    [ADD] Col, Row and input in Profile page

--- Paperwork-Front-Web/src/pages/Profile.js
+++ Paperwork-Front-Web/src/pages/Profile.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { MdAddAPhoto } from "react-icons/md";
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { FaCalendarAlt } from "react-icons/fa";
+import { Center, Grid, GridItem, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import "../styles/Profile.css"
 
 function Profile() {
+    const [showEyePwd, setShowEyePwd] = React.useState(false)
+    const handleClickEyePwd = () => setShowEyePwd(!showEyePwd)
+
     return (
         <>
-            <p>Profile Page</p>
-                <MdAddAPhoto />
-            <AiFillEyeInvisible />
-            <AiFillEye />
-                <FaCalendarAlt />
-                Calendar
+            <Grid h='600px' templateRows='repeat(2, 1fr)' templateColumns='repeat(5, 1fr)' gap={4} >
+                <GridItem rowSpan={2} colSpan={2} bg='blue'>
+                    <Center>
+                        <InputGroup size='md' width={"auto"}>
+                            <Input
+                                className='Profile-Input'
+                                pr='4.5rem'
+                                type={showEyePwd ? 'text' : 'password'}
+                                placeholder='Enter password'
+                            />
+                            <InputRightElement>
+                                <Button className='Profile-ShowPwdBtn' size='sm' onClick={handleClickEyePwd}>
+                                {
+                                    showEyePwd ? <AiFillEye className='Profile-Input-Icon' size={20} /> : <AiFillEyeInvisible className='Profile-Input-Icon' size={20} />
+                                }
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                    </Center>
+                </GridItem>
+                <GridItem rowSpan={1} colSpan={3} bg='red'>
+
+                </GridItem>
+                <GridItem rowSpan={1} colSpan={3} bg='green'>
+
+                </GridItem>
+            </Grid>
         </>
     );
 }
