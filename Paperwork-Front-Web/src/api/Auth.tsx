27c6632baeefd61e7727fe2e8b21fec4509652c9
Anne <anne.mboke@epitech.eu>
2022-06-18 16:38:10 +0200

    [DEL] old repository

--- Paperwork-Front-Web/src/api/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Auth = () => {
-    return (
-        <p>hihihi</p>
-    );
-}
-
-export default Auth;
--- Paperwork-Front-Web/src/pages/Error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => {
-    return (
-        <p>Error 404</p>
-    );
-}
-
-export default ErrorPage;
