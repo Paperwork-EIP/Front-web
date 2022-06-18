08a0b138e548622289caa08caca04e605ec10be2
Austin-William Lo <austin-william.lo@epitech.eu>
2022-06-18 22:27:27 +0200

    [FIX] CalendarButton integratrion tests

--- paperwork-front-web/src/test/container/Profile/CalendarButton.test.tsx
+++ paperwork-front-web/src/test/container/Profile/CalendarButton.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import '@testing-library/jest-dom';
 import { BrowserRouter } from "react-router-dom";
 import { render, screen, configure } from "@testing-library/react";
 import user from "@testing-library/user-event";
@@ -8,25 +8,25 @@ import CalendarButton from "../../../container/Profile/CalendarButton";
 beforeEach(() => {
     configure({
         throwSuggestions: true,
-    })
+    });
+    render(
+        <BrowserRouter>
+            <CalendarButton />
+        </BrowserRouter>
+    );
 })
 
 /////////////////////////// VALID CASE TESTS ///////////////////////////
 
 describe("Calendar button component", () => {
-    it("renders without crashing", () => {
-        render(
-            <BrowserRouter>
-                <CalendarButton />
-            </BrowserRouter>
-        );
-    });
+    test("renders without crashing", () => { });
 });
 
-describe("Units tests", () => {
+describe("Valid units tests", () => {
     test('should have a button', () => {
         const button = screen.getByRole('button', { name: /calendar-button/i });
 
+        expect(button).toBeVisible();
         expect(button).toBeInTheDocument();
         expect(button).toHaveClass('chakra-button');
         expect(button).toHaveClass('Profile-CalendarButton');
@@ -34,17 +34,8 @@ describe("Units tests", () => {
     });
 });
 
-/////////////////////////// INVALID CASE TESTS ///////////////////////////
-
-describe("Integration tests", () => {
+describe("Valid integration tests", () => {
     test('should redirect to the calendar page', () => {
-        render(
-            <BrowserRouter>
-                <CalendarButton />
-                {/* Pas de onClick pour l'instant */}
-            </BrowserRouter>
-        );
-
         const mockCallback = jest.fn();
         const button = screen.getByRole('button', { name: /calendar-button/i });
 
@@ -52,4 +43,15 @@ describe("Integration tests", () => {
 
         expect(mockCallback).toHaveBeenCalled();
     });
+
+    test('should do nothing when not clicked', () => {
+        const mockCallback = jest.fn();
+        const button = screen.getByRole('button', { name: /calendar-button/i });
+
+        user.click(button);
+
+        expect(mockCallback).not.toHaveBeenCalled();
+    });
 });
+
+/////////////////////////// INVALID CASE TESTS ///////////////////////////
