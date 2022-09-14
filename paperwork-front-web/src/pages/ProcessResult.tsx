445c24fddfc7700b38e4b009647f9db1bcd48462
DylanFaure <dylan1.faure@epitech.eu>
2022-09-14 05:34:36 +0200

    [ADD] Button link 'Start a new process'

--- paperwork-front-web/src/pages/ProcessResult.tsx
+++ paperwork-front-web/src/pages/ProcessResult.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import { Box, Heading, Icon } from '@chakra-ui/react';
+import { Box, Heading, Icon, Button } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
 import Header from '../components/Header';
 import { AiFillCheckSquare } from "react-icons/ai";
 import { BsSquareFill } from "react-icons/bs";
+import { FaLessThan } from "react-icons/fa";
 
 function ProcessResult(props: any) {
     return (
         <>
             <Header/>
-            <Box bg='#E0FDF7' p={5} m={50} color='black'>
-                <Heading mb={12} as='u'>Result of the process for “{props.processInfo.type}”:</Heading>
-                {
-                    props.processInfo.tasks?.map((item: any) => {
-                        return (
-                            item.state === true ?
-                            <p className='ProcessResult-State'><Icon as={AiFillCheckSquare} size={50} color="#29C9B3"/> : {item.description}</p>
-                            :
-                            <p className='ProcessResult-State'><Icon as={BsSquareFill} size={40} color="#FFFFFF" /> : {item.description}</p>
-                        );
-                    })
-                }
+            <Box p={5} m={50} color='black'>
+                <Button variant='link' leftIcon={ <FaLessThan size={10} /> } color="#000000" size='lg'><Link to="/newProcessLink">Start a new process</Link></Button>
+                <Box p={5} bg='#E0FDF7'>
+                    <Heading mb={12} as='u'>Result of the process for “{props.processInfo.type}”:</Heading>
+                    <Box pt={9}>
+                    {
+                        props.processInfo.tasks?.map((item: any) => {
+                            return (
+                                item.state === true ?
+                                <Box pb={1}><Icon as={ AiFillCheckSquare } size={50} color="#29C9B3"/> : {item.description}</Box>
+                                :
+                                <Box pb={1}><Icon as={ BsSquareFill } size={40} color="#FFFFFF" /> : {item.description}</Box>
+                            );
+                        })
+                    }
+                    </Box>
+                </Box>
             </Box>
         </>
     );
