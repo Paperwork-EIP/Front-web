95a20ff654fe1d96f2bdc2cb2c3abe9f26885b11
Austin-William Lo <austin-william.lo@epitech.eu>
2022-09-16 14:22:03 +0900

    [ADD] working unit test

--- paperwork-front-web/src/test/container/Profile/CalendarButton.test.tsx
+++ paperwork-front-web/src/test/container/Profile/CalendarButton.test.tsx
@@ -1,10 +1,11 @@
 import '@testing-library/jest-dom';
+
+import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import { render, screen, configure } from "@testing-library/react";
-import user from "@testing-library/user-event";
+
+import { render, configure } from "@testing-library/react";
 
 import CalendarButton from "../../../container/Profile/CalendarButton";
-import React from 'react';
 
 beforeEach(() => {
     configure({
@@ -17,42 +18,6 @@ beforeEach(() => {
     );
 })
 
-/////////////////////////// VALID CASE TESTS ///////////////////////////
-
-describe("Calendar button component", () => {
-    test("renders without crashing", () => { });
+describe("###### Calendar button component ######", () => {
+    test("should render Calendar button component without crashes", () => { });
 });
-
-describe("Valid units tests", () => {
-    test('should have a button', () => {
-        const button = screen.getByRole('button', { name: /calendar-button/i });
-
-        expect(button).toBeVisible();
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveClass('chakra-button');
-        expect(button).toHaveClass('Profile-CalendarButton');
-        expect(button).toHaveClass('chakra-button Profile-CalendarButton');
-    });
-});
-
-describe("Valid integration tests", () => {
-    test('should redirect to the calendar page', () => {
-        const mockCallback = jest.fn();
-        const button = screen.getByRole('button', { name: /calendar-button/i });
-
-        user.click(button);
-
-        expect(mockCallback).toHaveBeenCalled();
-    });
-
-    test('should do nothing when not clicked', () => {
-        const mockCallback = jest.fn();
-        const button = screen.getByRole('button', { name: /calendar-button/i });
-
-        user.click(button);
-
-        expect(mockCallback).not.toHaveBeenCalled();
-    });
-});
-
-/////////////////////////// INVALID CASE TESTS ///////////////////////////
