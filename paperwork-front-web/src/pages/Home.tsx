e97bc6ebe2e23802b812e62659857b7f07999762
Anne <anne.mboke@epitech.eu>
2022-09-27 03:42:20 +0200

    [ADD] home page content

--- paperwork-front-web/src/pages/Home.tsx
+++ paperwork-front-web/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import Header from '../components/Header';
+import Bg from '../container/Home/HomeContent';
+import GridBlurredBackdrop from '../container/Home/HomeContent';
 
 const HomePage = () => {
     return (
-        <Header/>
+        <>
+            <Header/>
+            <Bg/>
+        </>
     );
 }
 
