9e0e1a944b70305f194694427018658564025609
LucaBanyols <luca.banyols@epitech.eu>
2023-03-29 21:23:50 +0900

    [FIX] Sonar Build Errors

--- paperwork-front-web/src/pages/ForgotPassword.tsx
+++ paperwork-front-web/src/pages/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Cookies from 'universal-cookie';
 import "../styles/ForgotPassword.css";
