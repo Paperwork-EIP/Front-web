aeae01e762bd75d9d9ebe12eb7bef0e9a0ffe743
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2023-04-12 02:05:16 +0900

    [ADD] home tests and remove useless files

--- paperwork-front-web/src/pages/Home.tsx
+++ paperwork-front-web/src/pages/Home.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Header from '../components/Header';
-import Bg from '../container/Home/HomeContent';
+
 import Cookies from 'universal-cookie';
 
-const cookies = new Cookies();
-// import GridBlurredBackdrop from '../container/Home/HomeContent';
+import Header from '../components/Header';
+import Bg from '../container/Home/HomeContent';
 
-const HomePage = (props: any) => {
+const cookies = new Cookies();
+
+function HomePage() {
     if (!cookies.get('loginToken')) {
-        window.location.assign('/');
+        window.location.replace('/');
     }
+
     return (
         <>
-            <Header/>
-            <Bg/>
+            <Header />
+            <Bg />
         </>
     );
 }
