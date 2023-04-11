2893325a52292d9049c867ec269a21ed7e0de439
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2023-04-12 02:12:27 +0900

    [REMOVE] useless files

--- paperwork-front-web/src/index.tsx
+++ paperwork-front-web/src/index.tsx
@@ -1,8 +1,9 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 import { BrowserRouter } from "react-router-dom";
+
+import App from './App';
 
 ReactDOM.render(
   <StrictMode>
