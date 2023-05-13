f51205f81bdf9853eeffc3cdeed4da5f9a1cc96a
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2023-05-14 01:50:29 +0900

    [FIX] tests

--- paperwork-front-web/src/pages/Home.tsx
+++ paperwork-front-web/src/pages/Home.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import Cookies from 'universal-cookie';
-
 import Header from '../components/Header';
 import Bg from '../container/Home/HomeContent';
 
-const cookies = new Cookies();
-
 function HomePage() {
-    if (!cookies.get('loginToken')) {
-        window.location.replace('/');
-    }
-
     return (
         <>
             <Header />
