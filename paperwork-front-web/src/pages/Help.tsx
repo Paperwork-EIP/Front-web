5b5d0f218103ba0e49391e8bf06a224fb1ab5906
LucaBanyols <luca.banyols@epitech.eu>
2023-06-27 02:27:29 +0900

    [ADD] Lexicon Page and Updated Help page

--- paperwork-front-web/src/pages/Help.tsx
+++ paperwork-front-web/src/pages/Help.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Cookies from 'universal-cookie';
-import "../styles/Help.css";
+import "../styles/Help.scss";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { getTranslation } from './Translation';
+import { useColorMode } from '@chakra-ui/react';
 
 interface FAQ {
   question: string;
   answer: string;
-  image: string;
+  imageLight: string;
+  imageDark: string;
   alt: string;
   link: string;
   open: boolean;
@@ -25,6 +27,9 @@ function HelpPage() {
   }
 
   const cookieList = cookies.get('loginToken');
+  const { colorMode } = useColorMode();
+  const translation = getTranslation(language, "helpPage");
+
 
   useEffect(() => {
     axios.get(`${api}/user/getbytoken`, { params: { token: cookieList.loginToken } })
@@ -37,12 +42,12 @@ function HelpPage() {
   }, [cookieList]);
 
   useEffect(() => {
-    const translation = getTranslation(language, "helpPage");
     setFaqs([
       {
         question: translation.question1,
         answer: translation.answer1,
-        image: "assets/help-page/Profile-neutral.png",
+        imageLight: "assets/help-page/Profile-light.png",
+        imageDark: "assets/help-page/Profile-dark.png",
         alt: "Profile_page_clickable_image",
         link: "/profile",
         open: false
@@ -50,7 +55,8 @@ function HelpPage() {
       {
         question: translation.question2,
         answer: translation.answer2,
-        image: "assets/help-page/Quiz-neutral.png",
+        imageLight: "assets/help-page/Quiz-light.png",
+        imageDark: "assets/help-page/Quiz-dark.png",
         alt: "Quiz_page_clickable_image",
         link: "/quiz",
         open: false
@@ -58,7 +64,8 @@ function HelpPage() {
       {
         question: translation.question3,
         answer: translation.answer3,
-        image: "assets/help-page/Calendar-create.png",
+        imageLight: "assets/help-page/Calendar-create-light.png",
+        imageDark: "assets/help-page/Calendar-create-dark.png",
         alt: "Calendar_addEvent_page_clickable_image",
         link: "/calendar",
         open: false
@@ -66,7 +73,8 @@ function HelpPage() {
       {
         question: translation.question4,
         answer: translation.answer4,
-        image: "assets/help-page/Calendar-delete.png",
+        imageLight: "assets/help-page/Calendar-delete-light.png",
+        imageDark: "assets/help-page/Calendar-delete-dark.png",
         alt: "Calendar_deleteEvent_page_clickable_image",
         link: "/calendar",
         open: false
@@ -74,9 +82,28 @@ function HelpPage() {
       {
         question: translation.question5,
         answer: translation.answer5,
-        image: "assets/help-page/Processidea-neutral.png",
+        imageLight: "assets/help-page/ProcessIdea-light.png",
+        imageDark: "assets/help-page/ProcessIdea-dark.png",
         alt: "ProcessIdea_page_clickable_image",
         link: "/processidea",
+        open: false
+      },
+      {
+        question: translation.question6,
+        answer: translation.answer6,
+        imageLight: "assets/help-page/Settings-light.png",
+        imageDark: "assets/help-page/Settings-dark.png",
+        alt: "Settings_page_clickable_image",
+        link: "/settings",
+        open: false
+      },
+      {
+        question: translation.question7,
+        answer: translation.answer7,
+        imageLight: "assets/help-page/Lexicon-light.png",
+        imageDark: "assets/help-page/Lexicon-dark.png",
+        alt: "Lexicon_page_clickable_image",
+        link: "/lexicon",
         open: false
       }
     ]);
@@ -99,25 +126,50 @@ function HelpPage() {
   return (
     <>
       <Header />
-      <div className="faq-image">
-        <img src="assets/help-page/FAQs-bro.svg" alt="FAQs_bro_image" />
-      </div>
-      <div className="App">
-        <div className="faqs">
+      { colorMode === "light" ?
+        <><div className='Lexicon-text-button-light'>
+          {translation.textLexicon}
+        </div><Link className='Lexicon-button-light' to="/lexicon">{translation.buttonLexicon}</Link><div className="faq-light-image">
+            <img src="assets/help-page/FAQs-bro.svg" alt="FAQs_bro_image" />
+          </div></>
+        :
+        <><div className='Lexicon-text-button-dark'>
+          {translation.textLexicon}
+        </div><Link className='Lexicon-button-dark' to="/lexicon">{translation.buttonLexicon}</Link><div className="faq-dark-image">
+            <img src="assets/help-page/FAQs-bro.svg" alt="FAQs_bro_image" />
+          </div></>
+      }
+      { colorMode === "light" ?
+        <div className="faqs-light">
           {faqs.map((faq, index) => (
             <div data-testid="faq-button"
-              className={"faq " + (faq.open ? "open" : "")}
+              className={"faq-light " + (faq.open ? "open" : "")}
               key={index}
               onClick={() => toggleFAQ(index)}>
-              <div className="faq-question">{faq.question}</div>
-              <div className="faq-answer faq-answer-divider faq-display-linebreak">{faq.answer}</div>
+              <div className="faq-light-question">{faq.question}</div>
+              <div className="faq-light-answer faq-light-answer-divider faq-light-display-linebreak">{faq.answer}</div>
               <Link to={faq.link}>
-                <img className="faq-answer faq-screen" src={faq.image} alt={faq.alt} />
+                <img className="faq-light-answer faq-light-screen" src={faq.imageLight} alt={faq.alt} />
               </Link>
             </div>
           ))}
         </div>
-      </div>
+        :
+        <div className="faqs-dark">
+          {faqs.map((faq, index) => (
+            <div data-testid="faq-button"
+              className={"faq-dark " + (faq.open ? "open" : "")}
+              key={index}
+              onClick={() => toggleFAQ(index)}>
+              <div className="faq-dark-question">{faq.question}</div>
+              <div className="faq-dark-answer faq-dark-answer-divider faq-dark-display-linebreak">{faq.answer}</div>
+              <Link to={faq.link}>
+                <img className="faq-dark-answer faq-dark-screen" src={faq.imageDark} alt={faq.alt} />
+              </Link>
+            </div>
+          ))}
+        </div>
+        }
       <a href="mailto:paperwork_2024@labeip.epitech.eu">
           <div className="faq-image-contact-us faq-contact-mail" >
               <img src="assets/help-page/ContactUs-bro.svg" alt="ContactUs_bro_image" />
