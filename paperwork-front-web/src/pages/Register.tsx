a08d90c72f2b670c00a50109230b7e512377953b
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2023-10-14 08:47:28 +0200

    [FIX] responsive display

--- paperwork-front-web/src/pages/Register.tsx
+++ paperwork-front-web/src/pages/Register.tsx
@@ -8,6 +8,7 @@ import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
 import Navbar from '../components/Navbar';
+import FooterNoConnected from '../components/Footer';
 
 import "../styles/pages/Register.scss";
 
@@ -89,51 +90,54 @@ const RegisterPage = () => {
     });
 
     return (
-        <div className="Register">
+        <>
             <Navbar />
-            <div className='Register-container'>
-                <div className='Register-wrapper'>
-                    <h1 className='Register-title'>{t('register.title')}</h1>
-                    <div className='Register-form'>
-                        <div className="Register-form-group field">
-                            <input type="email" className="Register-form-field" placeholder="Email" name="email" id='email' data-testid="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                            <label htmlFor="email" className="Register-form-label">{t('register.email')}</label>
+            <div className="Register">
+                <div className='Register-container'>
+                    <div className='Register-wrapper'>
+                        <h1 className='Register-title'>{t('register.title')}</h1>
+                        <div className='Register-form'>
+                            <div className="Register-form-group field">
+                                <input type="email" className="Register-form-field" placeholder="Email" name="email" id='email' data-testid="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                                <label htmlFor="email" className="Register-form-label">{t('register.email')}</label>
+                            </div>
+                            <div className="Register-form-group field">
+                                <input type="text" className="Register-form-field" placeholder="Username" name="username" id='username' data-testid="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                                <label htmlFor="username" className="Register-form-label">{t('register.username')}</label>
+                            </div>
+                            <div className="Register-form-group field">
+                                <input type="password" className="Register-form-field" placeholder="Password" name="password" id='password' data-testid="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                <label htmlFor="password" className="Register-form-label">{t('register.password')}</label>
+                            </div>
+                            <div className="Register-form-group field">
+                                <input type="password" className="Register-form-field" placeholder="Confirm password" name="confirmPassword" id='confirmPassword' data-testid="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
+                                <label htmlFor="confirmPassword" className="Register-form-label">{t('register.confirm_password')}</label>
+                            </div>
                         </div>
-                        <div className="Register-form-group field">
-                            <input type="text" className="Register-form-field" placeholder="Username" name="username" id='username' data-testid="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                            <label htmlFor="username" className="Register-form-label">{t('register.username')}</label>
-                        </div>
-                        <div className="Register-form-group field">
-                            <input type="password" className="Register-form-field" placeholder="Password" name="password" id='password' data-testid="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                            <label htmlFor="password" className="Register-form-label">{t('register.password')}</label>
-                        </div>
-                        <div className="Register-form-group field">
-                            <input type="password" className="Register-form-field" placeholder="Confirm password" name="confirmPassword" id='confirmPassword' data-testid="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
-                            <label htmlFor="confirmPassword" className="Register-form-label">{t('register.confirm_password')}</label>
-                        </div>
-                    </div>
-                    <button className={buttonDisabled ? 'Register-submit-button disabled' : 'Register-submit-button'} aria-label='button-register' onClick={() => { handleSubmit() }} disabled={buttonDisabled}>
-                        {t('register.button')}
-                    </button>
-                    <div className='Register-connections'>
-                        <button className='Register-button-api' data-testid="google-link" onClick={googleConnect}>
-                            <FaGoogle />
+                        <button className={buttonDisabled ? 'Register-submit-button disabled' : 'Register-submit-button'} aria-label='button-register' onClick={() => { handleSubmit() }} disabled={buttonDisabled}>
+                            {t('register.button')}
                         </button>
-                        <button className='Register-button-api' data-testid="facebook-link" onClick={facebookConnect}>
-                            <FaFacebook />
-                        </button>
-                    </div>
-                    <div className='Register-redirection-to-login'>
-                        <span>
-                            {t('register.yes_account')}
-                        </span>
-                        <span className='Register-link'>
-                            <Link to='/login' data-testid="link-login">{t('register.yes_account_click')}</Link>
-                        </span>
+                        <div className='Register-connections'>
+                            <button className='Register-button-api' data-testid="google-link" onClick={googleConnect}>
+                                <FaGoogle />
+                            </button>
+                            <button className='Register-button-api' data-testid="facebook-link" onClick={facebookConnect}>
+                                <FaFacebook />
+                            </button>
+                        </div>
+                        <div className='Register-redirection-to-login'>
+                            <span>
+                                {t('register.yes_account')}
+                            </span>
+                            <span className='Register-link'>
+                                <Link to='/login' data-testid="link-login">{t('register.yes_account_click')}</Link>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <FooterNoConnected />
+        </>
     );
 }
 
