9bf6546a7fff76fbdda41d0ba22afc69cea44c1d
DylanFaure <70261784+DylanFaure@users.noreply.github.com>
2023-12-20 15:59:34 +0100

    [ADD] UnderQuestion question to Quiz and QuizQuestion pages [FIX] QuizQuestion language title

--- paperwork-front-web/src/pages/ProcessResult.tsx
+++ paperwork-front-web/src/pages/ProcessResult.tsx
@@ -1,41 +1,61 @@
+// React Import
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+
+// Utils Import
 import axios from "axios";
 import Cookies from 'universal-cookie';
 import { toast } from 'react-toastify';
-import { FaLessThan } from "react-icons/fa";
-import { BsLink } from "react-icons/bs";
-import { useColorMode } from '@chakra-ui/react';
 
-import { getTranslation } from './Translation';
+// Pages Import
 import Header from '../components/Header';
-
 import "../styles/pages/ProcessResult.scss";
 
-function ProcessResult() {
+// Icons Import
+import { FaLessThan } from "react-icons/fa";
+import { BsLink } from "react-icons/bs";
+
+// Translation Import
+import { getTranslation } from './Translation';
+
+// Color mode
+import { useColorMode } from '@chakra-ui/react';
+
+const ProcessResult = () => {
+
     const api = process.env.REACT_APP_BASE_URL;
     const cookies = new Cookies();
     const [stepsAnswer, setStepsAnswer] = useState([]);
     const [title, setTitle] = useState("");
 
+    if (!cookies.get('loginToken')) {
+        window.location.assign('/');
+    }
     const cookiesInfo = cookies.get('loginToken');
 
     let { processSelected } = useParams();
+
+    // User informations
     const [language, setLanguage] = useState("");
+
+
+    // Translation
     const translation = getTranslation(language, "processResult");
+
+    // Color mode
     const { colorMode } = useColorMode();
 
-    async function getLanguage() {
+    async function getLanguageByToken() {
         await axios.get(`${api}/user/getbytoken`, { params: { token: cookiesInfo.loginToken } })
             .then(res => {
                 setLanguage(res.data.language);
             }).catch(err => {
-                console.error(err);
+                console.log(err)
             });
     }
 
     async function getUserSteps() {
-        axios.get(`${api}/userProcess/getUserSteps`, { params: { process_title: processSelected, user_token: cookiesInfo.loginToken } })
+        await axios.get(`${api}/userProcess/getUserSteps`, { params: { process_title: processSelected, user_token: cookiesInfo.loginToken } })
             .then(res => {
                 setStepsAnswer(res.data.response);
                 setTitle(res.data.title);
@@ -45,14 +65,18 @@ function ProcessResult() {
                     }
                 });
             }).catch(err => {
-                console.error(err);
-            });
+                console.log(err);
+            }
+        );
     }
 
     useEffect(() => {
-        getLanguage();
-        getUserSteps();
-    }, [processSelected, api, stepsAnswer]);
+        getLanguageByToken();
+
+        if (cookiesInfo) {
+            getUserSteps();
+        }
+    }, [cookiesInfo, processSelected, api, stepsAnswer]);
 
     const handleCheckboxClick = (step_id: any, is_done: any) => {
         let newStepsAnswer = [];
